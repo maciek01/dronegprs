5f67eb532ecc7aec004c3fd14cdc9df5359cad1e
maciek01 <maciek@kolesnik.org>
2016-12-12 05:48:59 +0000

    support for dronekit

--- droneclient/install.sh
+++ droneclient/install.sh
@@ -1,7 +1,28 @@
 
-sudo apt-get install python-pip
+sudo apt-get install python-pip python-dev
+sudo apt-get install screen python-wxgtk2.8 python-matplotlib python-opencv python-numpy python-dev libxml2-dev libxslt-dev
+
+
 sudo pip install httplib2
 sudo pip install pyserial
+sudo pip install pymavlink
+sudo pip install mavproxy
+sudo pip install dronekit
+#sudo pip install dronekit-sitl
+
+
+
+
+
+
+
+
+
 sudo ln -s /home/pi/dronegprs/droneclient/droneclientd.sh /etc/init.d/droneclientd
 
 sudo update-rc.d droneclientd defaults
+
+
+
+
+
--- droneclient/run.sh
+++ droneclient/run.sh
@@ -1,4 +1,4 @@
 cd /home/pi/dronegprs/droneclient
-#/usr/bin/python /home/pi/dronegprs/droneclient/client.py 2>&1 >>~/drone.log &
-/usr/bin/python /home/pi/dronegprs/droneclient/client.py 2>&1 >/dev/null &
+/usr/bin/python /home/pi/dronegprs/droneclient/client.py 2>&1 >drone.log &
+#/usr/bin/python /home/pi/dronegprs/droneclient/client.py 2>&1 >/dev/null &
 
