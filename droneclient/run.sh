712c5b779b7698923fccf41c6975cf7530df33c0
maciek01 <maciek@kolesnik.org>
2016-12-28 05:21:12 +0000

    dont log stdout

--- droneclient/run.sh
+++ droneclient/run.sh
@@ -1,5 +1,5 @@
 cd /home/pi/dronegprs/droneclient
-screen -d -m /home/pi/dronegprs/droneclient/mavproxy.sh 2>&1 >>drone.log &
-/usr/bin/python /home/pi/dronegprs/droneclient/Main.py 2>&1 >>drone.log &
-#/usr/bin/python /home/pi/dronegprs/droneclient/Main.py 2>&1 >/dev/null &
+screen -d -m /home/pi/dronegprs/droneclient/mavproxy.sh 2>&1 >>/dev/null &
+/usr/bin/python /home/pi/dronegprs/droneclient/Main.py 2>&1 >>/dev/null &
+#/usr/bin/python /home/pi/dronegprs/droneclient/Main.py 2>&1 >>/dev/null &
 
