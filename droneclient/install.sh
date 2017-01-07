8043625e488daac8f999fd1e2eb9a6e0fd821bc5
maciek01 <maciek@kolesnik.org>
2017-01-07 05:58:34 +0000

    support for gprs comms

--- droneclient/install.sh
+++ droneclient/install.sh
@@ -1,6 +1,5 @@
 
-sudo apt-get install python-pip python-dev
-sudo apt-get install screen python-wxgtk2.8 python-matplotlib python-opencv python-numpy python-dev libxml2-dev libxslt-dev
+sudo apt-get install python-pip python-dev screen python-wxgtk2.8 python-matplotlib python-opencv python-numpy python-dev libxml2-dev libxslt-dev ppp screen elinks
 
 
 sudo pip install httplib2
@@ -16,12 +15,14 @@ sudo pip install dronekit
 
 
 
+#install gprs
+sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB0 /etc/ppp/peers/fonaUSB0
+sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB1 /etc/ppp/peers/fonaUSB1
+sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB2 /etc/ppp/peers/fonaUSB2
+sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB3 /etc/ppp/peers/fonaUSB3
 
-
-
-
+#install client daemon
 sudo ln -s /home/pi/dronegprs/droneclient/droneclientd.sh /etc/init.d/droneclientd
-
 sudo update-rc.d droneclientd defaults
 
 
--- droneclient/run.sh
+++ droneclient/run.sh
@@ -1,5 +1,15 @@
 cd /home/pi/dronegprs/droneclient
+
+#start gprs
+
+sudo pon fonaUSB0
+
+#start mavlink
+
 screen -d -m /home/pi/dronegprs/droneclient/mavproxy.sh 2>&1 >>/dev/null &
+
+#start drone controller
+
 /usr/bin/python /home/pi/dronegprs/droneclient/Main.py 2>&1 >>/dev/null &
 #/usr/bin/python /home/pi/dronegprs/droneclient/Main.py 2>&1 >>/dev/null &
 
