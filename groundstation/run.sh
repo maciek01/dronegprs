a6bcc16fd631b32e5bd7cfcdd84bd733f620c40d
maciek01 <maciek@kolesnik.org>
2017-09-30 20:41:35 +0000

    rehost groundstation on docker enginx

--- groundstation/run.sh
+++ groundstation/run.sh
@@ -1,3 +1,9 @@
 
-cd /home/pi/dronegprs/groundstation
-nohup ./server.py 2>&1 >server.log &
+
+docker container prune --force
+docker run --name groundstation-nginx -v /home/pi/dronegprs/groundstation:/usr/share/nginx/html:ro -d -p 8000:80 nginx
+
+#cd /home/pi/dronegprs/groundstation
+#nohup ./server.py 2>&1 >server.log &
+
+
