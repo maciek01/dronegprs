6638cd7c5d55a2849480a5d02d46b364a459f4f1
maciek01 <maciek@kolesnik.org>
2017-11-12 04:01:17 +0000

    docker stuff

--- groundstation/run.sh
+++ groundstation/run.sh
@@ -1,8 +1,10 @@
 
 
-docker container prune --force
-docker run --name groundstation-nginx -v /home/pi/dronegprs/groundstation:/usr/share/nginx/html:ro -d -p 8000:80 nginx
-docker run --name pi-postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=pi -d -p 5432:5432 postgres
+#docker container prune --force
+
+#docker run --name groundstation-nginx --restart always -v /home/pi/dronegprs/groundstation:/usr/share/nginx/html:ro -d -p 8000:80 nginx
+#docker restart groundstation-nginx
+
 
 #cd /home/pi/dronegprs/groundstation
 #nohup ./server.py 2>&1 >server.log &
