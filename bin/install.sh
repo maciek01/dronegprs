a89baad552333f9b64996caa3e8524aee532a3b3
Maciek Kolesnik <maciek@kolesnik.org>
2017-12-15 21:28:06 -0500

    move code around

--- bin/install.sh
+++ bin/install.sh
@@ -16,15 +16,15 @@ sudo pip install --upgrade dronekit
 
 
 #install gprs
-sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB0 /etc/ppp/peers/fonaUSB0
-sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB1 /etc/ppp/peers/fonaUSB1
-sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB2 /etc/ppp/peers/fonaUSB2
-sudo ln -s /home/pi/dronegprs/droneclient/etc/ppp/peers/fonaUSB3 /etc/ppp/peers/fonaUSB3
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/fonaUSB0 /etc/ppp/peers/fonaUSB0
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/fonaUSB1 /etc/ppp/peers/fonaUSB1
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/fonaUSB2 /etc/ppp/peers/fonaUSB2
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/fonaUSB3 /etc/ppp/peers/fonaUSB3
 
 #install client daemon
-sudo ln -s /home/pi/dronegprs/droneclient/bin/droneclientd.sh /etc/init.d/droneclientd
-sudo ln -s /home/pi/dronegprs/droneclient/bin/mavlinkd.sh /etc/init.d/mavlinkd
-sudo ln -s /home/pi/dronegprs/droneclient/bin/fonad.sh /etc/init.d/fonad
+sudo ln -s /home/pi/dronegprs/bin/droneclientd.sh /etc/init.d/droneclientd
+sudo ln -s /home/pi/dronegprs/bin/mavlinkd.sh /etc/init.d/mavlinkd
+sudo ln -s /home/pi/dronegprs/bin/fonad.sh /etc/init.d/fonad
 sudo update-rc.d droneclientd defaults
 sudo update-rc.d mavlinkd defaults
 sudo update-rc.d fonad defaults
