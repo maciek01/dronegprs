9b92e00f2d94a2ab03f639a1bb9f1889bd59ade6
maciek01 <maciek@kolesnik.org>
2019-02-09 14:50:18 +0000

    conditionally turn off wifi

--- bin/startFona.sh
+++ bin/startFona.sh
@@ -22,21 +22,20 @@ fi
 #start gprs
 
 sleep 2
-echo "AT" >>/dev/ttyUSB0
-sleep 1
-echo "AT" >>/dev/ttyUSB0
-sleep 1
-echo "AT+CFUN=1,1" >>/dev/ttyUSB0
+echo "ATZ" >>/dev/ttyUSB2
+sleep 2
+echo "AT+CFUN=1,1" >>/dev/ttyUSB2
 sleep 5
 
 #sudo pon fonaUSB0 debug dump logfd 2 updetach
 #sudo pon fonaUSB0
 pon mobile-noauth-USB2
 
-sleep 5
+sleep 1
 
 IP=`/sbin/ip addr show ppp0 | grep peer | awk ' { print $4 } ' | sed 's/\/32//'`
 echo $IP
+echo $IP >/home/pi/ppp0-ip
 
 #sudo route del default
 #sudo route add default ppp0
