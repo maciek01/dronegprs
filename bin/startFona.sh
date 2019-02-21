9803625873f25a9e5e3f4edb26dacea0abacf31a
Maciek Kolesnik <maciek@kolesnik.org>
2019-02-20 20:24:11 -0500

    Update startFona.sh

--- bin/startFona.sh
+++ bin/startFona.sh
@@ -2,32 +2,24 @@
 
 #exec 1> >(logger -s -t $(basename $0)) 2>&1
 
-#clear state
-
+#cleanup wifi
 sudo ifconfig wlan0 up
 
 #wait for USB
-
 while [ `lsusb |grep Qualcomm|wc -l ` -eq '0' ]
 do
     sleep 1
 done
 
 #modem is up
-
-sleep 2
-
-
+sleep 1
 
 #start gprs
-
 #MODEM="$($HOME/dronegprs/src/getModem.py)"
 MODEM="/dev/ttyUSB2"
 
 echo "Modem id "$MODEM - ${MODEM: -4}
-
 echo $MODEM > $HOME/modemline
-
 if [ "$MODEM" == "" ]; then
 	echo "no modem line"
 	exit
@@ -35,33 +27,26 @@ else
       echo "modem found"
 fi
 
-sleep 2
+#reset and ping the modem
 echo "ATZ" >>$MODEM
 sleep 2
 echo "AT+CFUN=1,1" >>$MODEM
-sleep 5
+sleep 2
 
-#sudo pon fonaUSB0 debug dump logfd 2 updetach
-#sudo pon fonaUSB0
-
+#dial up
 pon mobile-noauth-${MODEM: -4}
-
 sleep 1
 
-
-#wait for USB
-
+#wait for ppp0
 while [ `ifconfig |grep ppp0|wc -l ` -eq '0' ]
 do
     sleep 1
 done
-
 sleep 5
 
 >$HOME/modemup
 
 #fixup dns and routing
-
 sudo cp /home/pi/dronegprs/resolv.conf.8.8.8.8 /etc/resolv.conf
 sudo cp /home/pi/dronegprs/resolv.conf.8.8.8.8 /etc/ppp/resolv.conf
 
@@ -70,7 +55,6 @@ sudo cp /home/pi/dronegprs/resolv.conf.8.8.8.8 /etc/ppp/resolv.conf
 #default         192.168.2.1     0.0.0.0         UG    304    0        0 wlan0
 #10.64.64.64     *               255.255.255.255 UH    0      0        0 ppp0
 #192.168.2.0     *               255.255.255.0   U     304    0        0 wlan0
-
 
 set +e
 sudo route delete default gw 192.168.2.1 wlan0
@@ -89,9 +73,6 @@ if [ ! -f $HOME/wifi ]; then
     sudo ifconfig wlan0 down
 fi
 
-
 IP=`/sbin/ip addr show ppp0 | grep peer | awk ' { print $4 } ' | sed 's/\/32//'`
 echo $IP
 echo $IP >$HOME/ppp0-ip
-
-
