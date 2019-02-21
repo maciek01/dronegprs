d62f4a903bb974a2c62d87213153f547dfb9958b
maciek01 <maciek@kolesnik.org>
2019-02-21 01:05:37 +0000

    replace default routes

--- bin/startFona.sh
+++ bin/startFona.sh
@@ -13,12 +13,6 @@ do
     sleep 1
 done
 
-#stop wifi
-if [ ! -f $HOME/wifi ]; then
-    echo "turn wifi off"
-    sudo ifconfig wlan0 down
-fi
-
 #modem is up
 
 sleep 2
@@ -66,8 +60,34 @@ sleep 5
 
 >$HOME/modemup
 
+#fixup dns and routing
+
 sudo cp /home/pi/dronegprs/resolv.conf.8.8.8.8 /etc/resolv.conf
 sudo cp /home/pi/dronegprs/resolv.conf.8.8.8.8 /etc/ppp/resolv.conf
+
+#default state
+#Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+#default         192.168.2.1     0.0.0.0         UG    304    0        0 wlan0
+#10.64.64.64     *               255.255.255.255 UH    0      0        0 ppp0
+#192.168.2.0     *               255.255.255.0   U     304    0        0 wlan0
+
+
+set +e
+sudo route delete default gw 192.168.2.1 wlan0
+sudo route add default gw 10.64.64.64 ppp0
+set -e
+
+#new state
+#Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+#default         10.64.64.64     0.0.0.0         UG    0      0        0 ppp0
+#10.64.64.64     *               255.255.255.255 UH    0      0        0 ppp0
+#192.168.2.0     *               255.255.255.0   U     304    0        0 wlan0
+
+#stop wifi
+if [ ! -f $HOME/wifi ]; then
+    echo "turn wifi off"
+    sudo ifconfig wlan0 down
+fi
 
 
 IP=`/sbin/ip addr show ppp0 | grep peer | awk ' { print $4 } ' | sed 's/\/32//'`
