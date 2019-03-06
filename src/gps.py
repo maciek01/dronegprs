85fc90b769b5a78178be94570d77c61581016eaa
maciek01 <maciek@kolesnik.org>
2019-03-06 04:20:59 +0000

    improved parsing of NMEA packets

--- src/gps.py
+++ src/gps.py
@@ -49,46 +49,61 @@ def handle_newline(line):
 	if line.startswith("$GPGLL") or line.startswith("$GNGLL") or line.startswith("$GLGLL"):
 		#print line
 		elements = line.split(',')
-		GPSLAT = line[7:9] + " " + line[9:17] + " " + line[18:19]
-		GPSLATNORM = (float(line[7:9]) + float(line[9:17]) / 60.0) * (1 if line[18:19].lower() == "n" else -1)
-		GPSLON = line[20:23] + " " + line[23:31] + " " + line[32:33]
-		GPSLONNORM = (float(line[20:23]) + float(line[23:31]) / 60.0) * (1 if line[32:33].lower() == "e" else -1)
-		GPSTIME = line[34:36] + ":" + line[36:38] + ":" + line[38:43]
-		if line[44:45] == "A":
+		lat = elements[1]
+		ns = elements[2]
+		lon = elements[3]
+		ew = elements[4]
+		gpstime = elements[5]
+		status = elements[6]
+
+		parseStdData(lat, ns, lon, ew, gpstime)
+
+		if status == "A":
 			GPSSTATUS = "VALID"
 		else:
 			GPSSTATUS = "INVALID"
 		GPSLASTSTATUSMS = current_milli_time()
+
 	if line.startswith("$GNGGA"):
                 #print line
 		elements = line.split(',')
-                GPSLAT = line[17:19] + " " + line[19:27] + " " + line[28:29]
-                GPSLATNORM = (float(line[17:19]) + float(line[19:27]) / 60.0) * (1 if line[28:29].lower() == "n" else -1)
-                GPSLON = line[30:33] + " " + line[33:41] + " " + line[42:43]
-                GPSLONNORM = (float(line[30:33]) + float(line[33:41]) / 60.0) * (1 if line[42:43].lower() == "e" else -1)
-                GPSTIME = line[7:9] + ":" + line[9:11] + ":" + line[11:16]
+		lat = elements[2]
+		ns = elements[3]
+		lon = elements[4]
+		ew = elements[5]
+		gpstime = elements[1]
+
+		parseStdData(lat, ns, lon, ew, gpstime)
+
 		GPSSTATUS = "UNKNOWN"
                 GPSLASTSTATUSMS = current_milli_time()
 		GPSALT=float(elements[9])
 		GPSNSATS = int(elements[7])
 		GPSFIX = int(elements[6])
+
 	if line.startswith("$GNRMC") and False:
                 #print line
 		elements = line.split(',')
-		GPSLAT = line[19:21] + " " + line[21:29] + " " + line[30:31]
-                GPSLATNORM = (float(line[19:21]) + float(line[21:29]) / 60.0) * (1 if line[30:31].lower() == "n" else -1)
-                GPSLON = line[32:35] + " " + line[35:43] + " " + line[44:45]
-                GPSLONNORM = (float(line[32:35]) + float(line[35:43]) / 60.0) * (1 if line[44:45].lower() == "e" else -1)
-                GPSTIME = line[7:9] + ":" + line[9:11] + ":" + line[11:16]
-                if line[17:18] == "A":
-                        GPSSTATUS = "VALID"
-                else:
-                        GPSSTATUS = "INVALID"
+		lat = elements[3]
+		ns = elements[4]
+		lon = elements[5]
+		ew = elements[6]
+		gpstime = elements[1]
+		status = elemenets[2]
+
+		parseStdData(lat, ns, lon, ew, gpstime)
+
+		if status == "A":
+			GPSSTATUS = "VALID"
+		else:
+			GPSSTATUS = "INVALID"
                 GPSLASTSTATUSMS = current_milli_time()
+
 		speed = float(elements[7])
 		heading = float(elements[8] if elements[8] != "" else 0)
 		GPSSPEED = speed * 0.514444 #knots to mps
 		GPSHEADNING = heading
+
 	if line.startswith("$GNVTG"):
                 #print line
 		elements = line.split(',')
@@ -97,6 +112,22 @@ def handle_newline(line):
 		GPSSPEED = speed * 0.277778 #kmph to mps
 		GPSHEADNING = heading
 
+def parseStdData(lat, ns, lon, ew, gpstime):
+
+	global GPSLAT
+	global GPSLATNORM
+	global GPSLON
+	global GPSLONNORM
+	global GPSTIME
+
+	if lat != "":
+		GPSLAT = lat[:2] + " " + lat[2:10] + " " + ns
+		GPSLATNORM = (float(lat[:2]) + float(lat[2:10]) / 60.0) * (1 if ns.lower() == "n" else -1)
+	if lon != "":
+		GPSLON = lon[:3] + " " + lon[3:11] + " " + ew
+		GPSLONNORM = (float(lon[:3]) + float(lon[3:11]) / 60.0) * (1 if ew.lower() == "e" else -1)
+	if gpstime != "":
+		GPSTIME = gpstime[:2] + ":" + gpstime[2:4] + ":" + gpstime[4:]
 
 def handle_data(data):
 	global buffer
