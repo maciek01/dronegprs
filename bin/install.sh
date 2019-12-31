8625a75386e76f5d308e0ca2dd8cc52950f894e5
Maciek Koleśnik <maciek@kolesnik.org>
2019-12-31 14:47:13 -0500

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -4,17 +4,33 @@
 
 #install pppd for wireless
 
-# 4G-hologram:
-sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-hologram-USB2 /etc/ppp/peers/mobile-noauth-hologram-USB2
-
-# 3G:
 sudo ln -s /home/pi/dronegprs/etc/ppp/chatscripts /etc/ppp/chatscripts
 sudo ln -s /home/pi/dronegprs/etc/ppp/options-mobile /etc/ppp/options-mobile
+
+# LTE-hologram:
+
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom7600a-hologram-USB2 /etc/ppp/peers/mobile-noauth-simcom7600a-hologram-USB2
+
+# LTE-att
 
 sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-USB0 /etc/ppp/peers/mobile-noauth-USB0
 sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-USB1 /etc/ppp/peers/mobile-noauth-USB1
 sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-USB2 /etc/ppp/peers/mobile-noauth-USB2
 sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-USB3 /etc/ppp/peers/mobile-noauth-USB3
+
+# 3G-hologram:
+
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB0 /etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB0
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB1 /etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB1
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB2 /etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB2
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB3 /etc/ppp/peers/mobile-noauth-simcom5320a-hologram-USB3
+
+# 3G-att:
+
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-att-USB0 /etc/ppp/peers/mobile-noauth-simcom5320a-att-USB0
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-att-USB1 /etc/ppp/peers/mobile-noauth-simcom5320a-att-USB1
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-att-USB2 /etc/ppp/peers/mobile-noauth-simcom5320a-att-USB2
+sudo ln -s /home/pi/dronegprs/etc/ppp/peers/mobile-noauth-simcom5320a-att-USB3 /etc/ppp/peers/mobile-noauth-simcom5320a-att-USB3
 
 # 2G:
 sudo ln -s /home/pi/dronegprs/etc/ppp/peers/fonaUSB0 /etc/ppp/peers/fonaUSB0
@@ -25,11 +41,8 @@ sudo ln -s /home/pi/dronegprs/etc/ppp/peers/fonaUSB3 /etc/ppp/peers/fonaUSB3
 #install client daemon
 
 sudo ln -s /home/pi/dronegprs/bin/droneclientd.sh /etc/init.d/droneclientd
-#mavlink is only necessary to bridge over between on board and offboard computers
-#sudo ln -s /home/pi/dronegprs/bin/mavlinkd.sh /etc/init.d/mavlinkd
 sudo ln -s /home/pi/dronegprs/bin/fonad.sh /etc/init.d/fonad
 sudo update-rc.d droneclientd defaults
-#sudo update-rc.d mavlinkd defaults
 sudo update-rc.d fonad defaults
 
 sudo usermod -a -G dip pi
