3d4b1c6fe478aeaf33cc24f0942f10374a37ebdf
Maciek Koleśnik <maciek@kolesnik.org>
2020-02-01 21:50:11 -0500

    Update update-pi.sh

--- bin/update-pi.sh
+++ bin/update-pi.sh
@@ -8,7 +8,7 @@ sudo apt-get autoclean -y
 sudo apt-get remove -y
 sudo apt autoremove -y
 
-sudo apt-get install git python-pip python-dev screen python-wxgtk2.8 python-matplotlib python-opencv python-numpy libxml2-dev libxslt-dev ppp elinks python3-lxml python-lxml dnsutils cu sqlite3 python-pysqlite2 ffmpeg gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-plugins-bad -y
+sudo apt-get install git python-pip python-dev screen python-wxgtk2.8 python-matplotlib python-opencv python-numpy libxml2-dev libxslt-dev ppp elinks python3-lxml python-lxml dnsutils cu sqlite3 python-pysqlite2 ffmpeg gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly -y
 
 sudo pip install --upgrade future
 sudo pip install --upgrade httplib2
