cd2071264e585707084c7afe60e1c0ec1cde90bb
maciek01 <maciek@kolesnik.org>
2021-01-01 16:53:50 +0000

    minor cleanup

--- bin/update-pi.sh
+++ bin/update-pi.sh
@@ -12,10 +12,8 @@ sudo apt-get install git python-pip python-dev screen python-wxgtk2.8 python-mat
 sudo apt-get install libgstreamer1.0-0 libgstreamer1.0-0-dbg libgstreamer1.0-dev liborc-0.4-0 liborc-0.4-0-dbg liborc-0.4-dev liborc-0.4-doc gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 gstreamer1.0-alsa gstreamer1.0-doc gstreamer1.0-omx gstreamer1.0-plugins-bad gstreamer1.0-plugins-bad-dbg gstreamer1.0-plugins-bad-doc gstreamer1.0-plugins-base gstreamer1.0-plugins-base-apps gstreamer1.0-plugins-base-dbg gstreamer1.0-plugins-base-doc gstreamer1.0-plugins-good gstreamer1.0-plugins-good-dbg gstreamer1.0-plugins-good-doc gstreamer1.0-plugins-ugly gstreamer1.0-plugins-ugly-dbg gstreamer1.0-plugins-ugly-doc gstreamer1.0-pulseaudio gstreamer1.0-tools gstreamer1.0-x libgstreamer-plugins-bad1.0-0 libgstreamer-plugins-bad1.0-dev libgstreamer-plugins-base1.0-0 libgstreamer-plugins-base1.0-dev -y
 
 
-sudo pip install --upgrade future
-sudo pip install --upgrade httplib2
-sudo pip install --upgrade requests
-sudo pip install --upgrade pyserial
+sudo pip install --upgrade future httplib2 requests pyserial
+
 #sudo pip2 install --upgrade dronekit
 
 
